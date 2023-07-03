935a4c339b5ceae6680601c40399ba2b2acfa28d
jiangjie <509745383@qq.com>
2023-07-03 22:58:32 +0800

    final scene final version less time

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -5,7 +5,6 @@ mod data;
 mod ray;
 mod render;
 mod sphere;
-//mod test_scene;
 mod aarec;
 mod boxx;
 mod constant_medium;
@@ -16,8 +15,9 @@ mod vec3;
 mod world;
 
 use image::RgbImage; //接收render传回来的图片，在main中文件输出
-use indicatif::ProgressBar; //main中产生进度条并传给render
+ //main中产生进度条并传给render
 use std::fs::File;
+use std::time::Instant;
 
 use camera::Camera;
 use data::Data; //数据层
@@ -26,7 +26,7 @@ use vec3::Vec3;
 
 use std::process::Command;
 
-const AUTHOR: &str = "停云别叫恩公,叫___";
+const AUTHOR: &str = "seeker🤯";
 fn is_ci() -> bool {
     option_env!("CI").unwrap_or_default() == "true"
 }
@@ -34,12 +34,25 @@ fn is_ci() -> bool {
 fn main() {
     //f64::INFINITY;std::f64::consts::PI;
     // get environment variable CI, which is true for GitHub Actions
+    let now = Instant::now();
+    println!("🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈");
+    println!("游戏开始");
 
     let is_ci: bool = is_ci();
+    let path = "output/test_9t.jpg";
+    let height = 800;
+    let width = 800;
+    let gamma = 2;
+    let sample_times = 250;
+    let depth = 50;
+    println!("🚩图片参数");
     println!("CI: {}", is_ci);
-    let path = "output/test.jpg";
+    println!("图片大小:{}*{}",height,width);
+    println!("伽马值:{}",gamma);
+    println!("每个像素点采样次数:{}",sample_times);
+    println!("反射次数:{}",depth);
     //let data = Data::new(1000, 1500, 60, 2, 100, 40);
-    let data = Data::new(800, 800, 60, 2, 1000, 50);
+    let data = Data::new(height, width, 60, gamma, sample_times, depth);
     let origin = Vec3::new(478.0, 278.0, -600.0);
     let lookat = Vec3::new(278.0, 278.0, 0.0);
     let camera0 = Camera::new(
@@ -51,16 +64,11 @@ fn main() {
         0.0,
         10.0,
     );
-    let bar: ProgressBar = if is_ci {
-        ProgressBar::hidden()
-    } else {
-        ProgressBar::new((data.height * data.width) as u64)
-    };
-
-    let img: RgbImage = render(&data, camera0, bar); //data一定要引用
-                                                     // return ;
-                                                     // Output image to file
-    println!("Ouput image as \"{}\"\n Author: {}", path, AUTHOR);
+    let threadnum = 9;
+    println!("🤡渲染线程数:{}",threadnum);
+    let img: RgbImage = render(&data, camera0, is_ci,threadnum); //data一定要引用
+    // Output image to file                                                 
+    println!("💌Ouput image as \"{}\"\n😻Author: {}", path, AUTHOR);
     let output_image: image::DynamicImage = image::DynamicImage::ImageRgb8(img);
     let mut output_file: File = File::create(path).unwrap();
     match output_image.write_to(
@@ -77,4 +85,5 @@ fn main() {
             .output()
             .expect("failed to execute process");
     }
+    println!("🤖程序运行{}秒",now.elapsed().as_secs());
 }
--- raytracer/src/render.rs
+++ raytracer/src/render.rs
@@ -14,11 +14,10 @@ pub use crate::util::{
 pub use crate::vec3::Vec3;
 
 use image::{ImageBuffer, Rgb, RgbImage};
-use indicatif::ProgressBar;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use rand::{rngs::ThreadRng, Rng};
 use std::sync::{Arc, Mutex};
 use std::thread;
-
 pub fn ray_color(
     r: Ray,
     bvh_tree: &BvhNode,
@@ -212,7 +211,7 @@ pub fn pixel_color(
     ray_color(r, bvh_tree, depth, perlin, earth)
 }
 
-pub fn render(data: &Data, camera: Camera, bar: ProgressBar) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+pub fn render(data: &Data, camera: Camera, is_ci: bool,threadnum:usize) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let width = data.width;
     let height = data.height;
     let depth = data.depth;
@@ -226,65 +225,88 @@ pub fn render(data: &Data, camera: Camera, bar: ProgressBar) -> ImageBuffer<Rgb<
     // return img;
     let perlin = Perlin::init();
     let earth = ImageTexture::new("earthmap.jpg");
+
+    let multipb = MultiProgress::new();
+
+
     //bvh_tree.info();
     //有个data也要共享内存，要么就把值都提取出来
     //上互斥锁
     let image = Arc::new(Mutex::new(img));
     let cam = Arc::new(camera);
     //let sph_lst = Arc::new(sphere_list);
-    let bar = Arc::new(bar);
+    // let bar = Arc::new(bar);
     let bvh_tree = Arc::new(bvh_tree);
     let perlin = Arc::new(perlin);
     let earth = Arc::new(earth);
+    let multipb = Arc::new(multipb);
 
     let mut handles = vec![];
-    for j in 0..height {
+
+    for index  in 0..threadnum{
         //以下开始一个线程  对共享内存的复制
         let c = Arc::clone(&cam);
         //let sph = Arc::clone(&sph_lst);
-        let bar = Arc::clone(&bar);
+        // let bar = Arc::clone(&bar);
         let bvh_node = Arc::clone(&bvh_tree);
         let perlin = Arc::clone(&perlin);
         let earth = Arc::clone(&earth);
+        let mpb = Arc::clone(&multipb);
         //共享可写内存：上互斥锁
-        let image = Arc::clone(&image);
-        //开始线程
-        let handle = thread::spawn(move || {
-            for i in 0..width {
-                let mut sum_pixel_color: [f64; 3] = [0.0; 3];
+        let image = Arc::clone(&image); 
+        let handle = thread::spawn(move ||{
+            let bar = mpb.add(ProgressBar::new((height * width/threadnum) as u64));
+            let style = ProgressStyle::with_template(
+                "[{elapsed_precise}] {bar:40.red/blue} {pos:>10}/{len:10} {msg}",
+            )
+            .unwrap()
+            .progress_chars("$>");
+            bar.set_style(style);
+            for j in 0..height{
+                for i in 0..width{
+                    if (j*height+i)%threadnum == index{
+                        let mut sum_pixel_color: [f64; 3] = [0.0; 3];
 
-                for _k in 0..sample {
-                    let tmp_pixel_color: [f64; 3] = pixel_color(
-                        (i, j),
-                        &c,
-                        &bvh_node,
-                        (width, height),
-                        depth,
-                        &perlin,
-                        &earth,
-                    );
-                    for i in 0..3 {
-                        sum_pixel_color[i] += tmp_pixel_color[i];
+                        for _k in 0..sample {
+                            let tmp_pixel_color: [f64; 3] = pixel_color(
+                                (i, j),
+                                &c,
+                                &bvh_node,
+                                (width, height),
+                                depth,
+                                &perlin,
+                                &earth,
+                            );
+                            for i in 0..3 {
+                                sum_pixel_color[i] += tmp_pixel_color[i];
+                            }
+                        }
+                        for element in &mut sum_pixel_color {
+                            *element = (*element / sample as f64).powf(1.0 / (gamma as f64));
+                        }
+                        let pixel_color: [u8; 3] =
+                            color(sum_pixel_color[0], sum_pixel_color[1], sum_pixel_color[2]);
+                        //(*bar).inc(1);  
+                        if !is_ci{
+                            bar.inc(1);  
+                        }  
+                        
+                        let mut img = image.lock().unwrap();
+                        write_color(pixel_color, &mut img, i, j);
                     }
                 }
-                for element in &mut sum_pixel_color {
-                    *element = (*element / sample as f64).powf(1.0 / (gamma as f64));
-                }
-                let pixel_color: [u8; 3] =
-                    color(sum_pixel_color[0], sum_pixel_color[1], sum_pixel_color[2]);
-                let mut img = image.lock().unwrap();
-                write_color(pixel_color, &mut img, i, j);
-                (*bar).inc(1);
             }
+            if !is_ci{
+                bar.finish();
+            }
+            //println!("线程{}运行结束,运行用时{}秒",index,now.elapsed().as_secs());
         });
-        //加入线程
         handles.push(handle);
     }
     //等待所有线程结束
     for i in handles {
         i.join().unwrap();
     }
-    bar.finish();
     let img = image.lock().unwrap();
     (*img).clone()
 }
